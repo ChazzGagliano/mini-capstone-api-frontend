6e37bc3c7d79f2192ca835f0dc502900ac47b1ec
Chazz Gagliano <cgags17285@gmail.com>
2023-05-18 12:49:14 -0400

    create full stack index

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,15 @@
-export function PhotosIndex() {
-  return (
-    <div>
-      <h1>All products</h1>
-    </div>
-  );
-}
+export function ProductsIndex(props) {
+      return (
+        <div>
+          <h1>All photos</h1>
+         {props.products.map((product) => (
+           <div key={product.id}>
+             <p>name: {product.name}</p>
+             <p>price: {product.price}</p>
+             <p>description: {product.description}</p>
+             <img src={product.image} />
+           </div>
+         ))}
+        </div>
+      );
+    }
